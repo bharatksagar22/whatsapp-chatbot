694d189d3f7ab1a0c6bbaae36f392c565277ef57
bharatksagar22 <bharat.ksagar22@gmail.com>
2025-06-27 22:52:47 +0530

    Update vite.config.js

--- whatsapp-chatbot-dashboard/vite.config.js
+++ whatsapp-chatbot-dashboard/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
   server: {
+    host: "0.0.0.0",
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: "0.0.0.0"
   },
   preview: {
+    host: "0.0.0.0",
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: "0.0.0.0"
   }
 });
